d8631168b56264f96725cee56371a02129ef4266
Hombre-x <gabisanpar@gmail.com>
2024-06-21 20:21:13 -0500

    Major changes
    Added:
    - `readWithCharset` function to read contents into file with custom `java.nio.file.Charset`
    
    Refactored:
    - `readAs` deleted custom error handling
    
    - `writeAs` and `appendAs` now re-use their `Byte` variant. Also, they don't depend more in `StreamEncoder` dependency
    
    - Scaladoc in numerous functions to let the user know they use UTF-8 as a codec for writing and reading
    
    Signature:
    - All functions the returned bytes in write and append now return nothing as the Pipe was getting rid of them anyway.

--- core/src/main/scala/io/chrisdavenport/shellfish/FilesOs.scala
+++ core/src/main/scala/io/chrisdavenport/shellfish/FilesOs.scala
@@ -21,30 +21,50 @@
 
 package io.chrisdavenport.shellfish
 
-import cats.syntax.all.*
 import cats.effect.IO
 
 import fs2.{Stream, Chunk}
-import fs2.interop.scodec.*
 import fs2.io.file.{Files, Path, Flags}
 
-import scodec.{Codec, codecs}
+import scodec.Codec
 import scodec.bits.ByteVector
+
+import java.nio.charset.Charset
 
 object FilesOs {
 
-  val files = Files[IO]
+  val files: Files[IO] = Files[IO]
 
   // Read operations:
 
   /**
-   * Reads the contents of the file at the path and returns it as a String.
+   * Reads the contents of the fileat the path using UTF-8 decoding. Returns it
+   * as a String loaded in memory.
+   *
    * @param path
    *   The path to read from
    * @return
    *   The file loaded in memory as a String
    */
   def read(path: Path): IO[String] = files.readUtf8(path).compile.string
+
+  /**
+   * Reads the contents of the file at the path using the provided charset.
+   * Returns it as a String loaded in memory.
+   *
+   * @param charset
+   *   The charset to use to decode the file
+   * @param path
+   *   The path to read from
+   * @return
+   *   The file loaded in memory as a String
+   */
+  def readWithCharset(charset: Charset)(path: Path): IO[String] =
+    files
+      .readAll(path)
+      .through(fs2.text.decodeWithCharset(charset))
+      .compile
+      .string
 
   /**
    * Reads the contents of the file at the path and returns it as a ByteVector.
@@ -57,38 +77,31 @@ object FilesOs {
     files.readAll(path).compile.to(ByteVector)
 
   /**
-   * Reads the contents of the file at the path and returns it line by line as a
-   * Vector of Strings.
+   * Reads the contents of the file at the path using UTF-8 decoding and returns
+   * it line by line as a List of Strings.
    *
    * @param path
    *   The path to read from
    * @return
    *   The file loaded in memory as a collection of lines of Strings
    */
-  def readLines(path: Path): IO[Vector[String]] =
-    files.readUtf8Lines(path).compile.toVector
+  def readLines(path: Path): IO[List[String]] =
+    files.readUtf8Lines(path).compile.toList
 
   /**
-   * Reads the contents of the file and loads it as a type `A` using the
-   * provided codec.
+   * Reads the contents of the file and deserializes its contents as `A` using
+   * the provided codec.
    * @param A
    *   The type to read the file as
    * @param path
    *   The path to read from
-   * @param codec
+   * @param Codec[A]
    *   The codec that translates the file contents into the type `A`
    * @return
    *   The file loaded in memory as a type `A`
-   * @throws java.lang.Exception
-   *   if the file cannot be read or the contents cannot be parsed into the type
-   *   `A`
    */
-  def readAs[A](path: Path)(implicit codec: Codec[A]): IO[A] =
-    readBytes(path)
-      .map(bytes => codec.decodeValue(bytes.bits).require)
-      .handleErrorWith { e =>
-        new Exception(s"Failed to read file at $path", e).raiseError[IO, A]
-      }
+  def readAs[A: Codec](path: Path): IO[A] =
+    readBytes(path).map(bytes => Codec[A].decodeValue(bytes.bits).require)
 
   /**
    * The function reads the contents of the file at the path and returns it as a
@@ -98,38 +111,27 @@ object FilesOs {
    * @return
    *   A Stream of Bytes
    */
-  def stream(path: Path): Stream[IO, Byte] = files.readAll(path)
-
-  /**
-   * The function reads the contents of the file at the `path` and returns it as
-   * a `Chunk[Bytes]`,
-   * @param path
-   *   The path to read from
-   * @return
-   *   The file loaded in memory as a Chunk of Bytes
-   */
-  def chunk(path: Path): IO[Chunk[Byte]] = files.readAll(path).compile.to(Chunk)
+  def readStream(path: Path): Stream[IO, Byte] = files.readAll(path)
 
   // Write operations:
 
   /**
-   * This function overwites the contents of the file at the path with the
-   * contents provided in form of a entire string loaded in memory.
+   * This function overwites the contents of the file at the path using UTF-8
+   * encoding with the contents provided in form of a entire string loaded in
+   * memory.
    *
    * @param path
    *   The path to write to
    * @param contents
    *   The contents to write to the file
-   * @return
-   *   The number of bytes written
    */
-  def write(path: Path)(contents: String): IO[Long] =
+  def write(path: Path)(contents: String): IO[Unit] =
     Stream
       .emit(contents)
       .covary[IO]
       .through(files.writeUtf8(path))
       .compile
-      .count
+      .drain
 
   /**
    * This function overwites the contents of the file at the path with the
@@ -139,37 +141,32 @@ object FilesOs {
    *   The path to write to
    * @param contents
    *   The contents to write to the file
-   * @return
-   *   The number of bytes written
    */
-  def writeBytes(path: Path)(contents: ByteVector): IO[Long] =
+  def writeBytes(path: Path)(contents: ByteVector): IO[Unit] =
     Stream
-      .emit(contents)
+      .chunk(Chunk.byteVector(contents))
       .covary[IO]
-      .through(StreamEncoder.many(codecs.bytes).toPipeByte)
       .through(files.writeAll(path))
       .compile
-      .count
+      .drain
 
   /**
-   * This function overwites the contents of the file at the path with the
-   * contents provided. Each content inside the vector is written as a line in
-   * the file.
+   * This function overwites the contents of the file at the path using UTF-8
+   * encoding with the contents provided. Each content inside the list is
+   * written as a line in the file.
    *
    * @param path
    *   The path to write to
    * @param contents
    *   The contents to write to the file
-   * @return
-   *   The number of bytes written
    */
-  def writeLines(path: Path)(contents: Vector[String]): IO[Long] =
+  def writeLines(path: Path)(contents: Seq[String]): IO[Unit] =
     Stream
       .emits(contents)
       .covary[IO]
       .through(files.writeUtf8Lines(path))
       .compile
-      .count
+      .drain
 
   /**
    * The functions writes the contents of the file at the path with the contents
@@ -182,19 +179,11 @@ object FilesOs {
    *   The path to write to
    * @param contents
    *   The contents to write to the file
-   * @param codec
+   * @param Codec[A]
    *   The codec that translates the type A into a ByteVector
-   * @return
-   *   The number of bytes written
    */
-  def writeAs[A](path: Path)(contents: A)(implicit codec: Codec[A]): IO[Long] =
-    Stream
-      .emit(contents)
-      .covary[IO]
-      .through(StreamEncoder.many(codec).toPipeByte)
-      .through(files.writeAll(path))
-      .compile
-      .count
+  def writeAs[A: Codec](path: Path)(contents: A): IO[Unit] =
+    writeBytes(path)(Codec[A].encode(contents).require.bytes)
 
   /**
    * Similar to `write`, but appends to the file instead of overwriting it.
@@ -204,16 +193,14 @@ object FilesOs {
    *   The path to write to
    * @param contents
    *   The contents to write to the file
-   * @return
-   *   The number of bytes written
    */
-  def append(path: Path)(contents: String): IO[Long] =
+  def append(path: Path)(contents: String): IO[Unit] =
     Stream
       .emit(contents)
       .covary[IO]
       .through(files.writeUtf8(path, Flags.Append))
       .compile
-      .count
+      .drain
 
   /**
    * Similar to `write`, but appends to the file instead of overwriting it.
@@ -223,37 +210,46 @@ object FilesOs {
    *   The path to write to
    * @param contents
    *   The contents to write to the file
-   * @return
-   *   The number of bytes written
    */
-  def appendBytes(path: Path)(contents: ByteVector): IO[Long] =
+  def appendBytes(path: Path)(contents: ByteVector): IO[Unit] =
     Stream
-      .emit(contents)
+      .chunk(Chunk.byteVector(contents))
       .covary[IO]
-      .through(StreamEncoder.many(codecs.bytes).toPipeByte)
       .through(files.writeAll(path, Flags.Append))
       .compile
-      .count
+      .drain
 
   /**
    * Similar to `write`, but appends to the file instead of overwriting it.
-   * Saves each line of the content at the end of the file in form of a Vector
-   * of Strings.
+   * Saves each line of the content at the end of the file in form of a List of
+   * Strings.
    *
    * @param path
    *   The path to write to
    * @param contents
    *   The contents to write to the file
-   * @return
-   *   The number of bytes written
    */
-  def appendLines(path: Path)(contents: Vector[String]): IO[Long] =
+  def appendLines(path: Path)(contents: Seq[String]): IO[Unit] =
     Stream
       .emits(contents)
       .covary[IO]
       .through(files.writeUtf8Lines(path, Flags.Append))
       .compile
-      .count
+      .drain
+
+  /**
+   * Similar to append, but appends a single line to the end file as a newline
+   * instead of overwriting it.
+   *
+   * Equivalent to `append(path)(s"\n$contents")`
+   *
+   * @param path
+   *   The path to write to
+   * @param contents
+   *   The contents to write to the file
+   */
+  def appendLine(path: Path)(contents: String): IO[Unit] =
+    appendLines(path)(List(contents))
 
   /**
    * Similar to `write`, but appends to the file instead of overwriting it.
@@ -265,17 +261,9 @@ object FilesOs {
    *   The path to write to
    * @param contents
    *   The contents to write to the file
-   * @param codec
+   * @param Codec[A]
    *   The codec that translates the type A into a ByteVector
-   * @return
-   *   The number of bytes written
    */
-  def appendAs[A](path: Path)(contents: A)(implicit codec: Codec[A]): IO[Long] =
-    Stream
-      .emit(contents)
-      .covary[IO]
-      .through(StreamEncoder.many(codec).toPipeByte)
-      .through(files.writeAll(path, Flags.Append))
-      .compile
-      .count
+  def appendAs[A: Codec](path: Path)(contents: A): IO[Unit] =
+    appendBytes(path)(Codec[A].encode(contents).require.bytes)
 }
