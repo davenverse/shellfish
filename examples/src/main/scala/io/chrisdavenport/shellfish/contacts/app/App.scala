6495d1e8d87c297211944fe24904715618f23e27
Hombre-x <gabisanpar@gmail.com>
2024-08-10 12:08:07 -0500

    Changed monad sequencing in `App`
    Now all use flatMap insead of mapN

--- examples/src/main/scala/io/chrisdavenport/shellfish/contacts/app/App.scala
+++ examples/src/main/scala/io/chrisdavenport/shellfish/contacts/app/App.scala
@@ -1,12 +1,29 @@
 package io.chrisdavenport.shellfish.contacts.app
 
-import cats.syntax.all.*
-import cats.effect.{IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp}
+import io.chrisdavenport.shellfish.contacts.cli.{Cli, Prompt}
+import io.chrisdavenport.shellfish.contacts.core.ContactManager
+import io.chrisdavenport.shellfish.contacts.domain.*
+import io.chrisdavenport.shellfish.contacts.domain.contact.*
 
-import io.chrisdavenport.shellfish.contacts.cli.Cli.*
 
-object App extends IOApp.Simple {
+object App extends IOApp {
 
-  def run: IO[Unit] = IO.println("Hello, World!")
-  
+  def run(args: List[String]): IO[ExitCode] = {
+    
+    val cm = ContactManager.impl
+    val cli = Cli.make(cm)
+    
+    IO.pure(Prompt.parsePrompt(args)).flatMap {
+      case Help => cli.help
+      case AddContact => cli.addContact
+      case RemoveContact(username) => cli.removeContact(username)
+      case SearchId(username) => cli.searchId(username)
+      case SearchName(name) => cli.searchName(name)
+      case SearchEmail(email) => cli.searchEmail(email)
+      case SearchNumber(number) => cli.searchNumber(number)
+      case ViewAll => cli.viewAll
+      case UpdateContact(username, flags) => cli.updateContact(username, flags)
+    } >> IO(ExitCode.Success)
+  }
 }
--- examples/src/main/scala/io/chrisdavenport/shellfish/contacts/cli/Cli.scala
+++ examples/src/main/scala/io/chrisdavenport/shellfish/contacts/cli/Cli.scala
@@ -5,32 +5,32 @@ import cats.syntax.all.*
 
 import io.chrisdavenport.shellfish.contacts.core.ContactManager
 import io.chrisdavenport.shellfish.contacts.domain.*
+import io.chrisdavenport.shellfish.contacts.domain.contact.*
+
 
 class Cli private (cm: ContactManager) {
 
   def addContact: IO[Unit] =
     for {
-      _ <- IO.print("Enter the username: ")
-      username = IO.readLine
-      _ <- IO.print("Enter the first name: ")
-      firstName = IO.readLine
-      _ <- IO.print("Enter the last name: ")
-      lastName = IO.readLine
-      _ <- IO.print("Enter the phone number: ")
-      phoneNumber = IO.readLine
-      _ <- IO.print("Enter the email: ")
-      email = IO.readLine
-      contact <- (username, firstName, lastName, phoneNumber, email).parMapN(
-        Contact.apply
-      )
+      _           <- IO.print("Enter the username: ")
+      username    <- IO.readLine
+      _           <- IO.print("Enter the first name: ")
+      firstName   <- IO.readLine
+      _           <- IO.print("Enter the last name: ")
+      lastName    <- IO.readLine
+      _           <- IO.print("Enter the phone number: ")
+      phoneNumber <- IO.readLine
+      _           <- IO.print("Enter the email: ")
+      email       <- IO.readLine
+      contact = Contact(username, firstName, lastName, phoneNumber, email)
       _ <- cm.addContact(contact).handleErrorWith {
         case ContactFound(username) =>
           IO.println(s"Contact $username already exists")
       }
-
     } yield ()
 
-  def removeContact(username: Username): IO[Unit] = cm.removeContact(username) >> IO.println(s"Contact $username removed")
+  def removeContact(username: Username): IO[Unit] =
+    cm.removeContact(username) >> IO.println(s"Contact $username removed")
 
   def searchId(username: Username): IO[Unit] =
     for {
@@ -44,34 +44,64 @@ class Cli private (cm: ContactManager) {
   def searchName(name: Name): IO[Unit] =
     for {
       contacts <- cm.searchName(name)
-      _ <- contacts.traverse_(c => IO.println(c.show))
+      _        <- contacts.traverse_(c => IO.println(c.show))
     } yield ()
 
   def searchEmail(email: Email): IO[Unit] =
     for {
       contacts <- cm.searchEmail(email)
-      _ <- contacts.traverse_(c => IO.println(c.show))
+      _        <- contacts.traverse_(c => IO.println(c.show))
     } yield ()
 
   def searchNumber(number: PhoneNumber): IO[Unit] =
     for {
       contacts <- cm.searchNumber(number)
-      _ <- contacts.traverse_(c => IO.println(c.show))
+      _        <- contacts.traverse_(c => IO.println(c.show))
     } yield ()
 
   def viewAll: IO[Unit] =
     for {
       contacts <- cm.getAll
-      _ <- contacts.traverse_(c => IO.println(c.show))
+      _        <- contacts.traverse_(c => IO.println(c.show))
     } yield ()
 
-  def updateContact(username: Username, options: List[Flag]): IO[Unit] = {
-    val modifiedContact = options.foldLeft()
-    
-    cm.updateContact(username)(c => c.copy())
-  }
+  def updateContact(username: Username, options: List[Flag]): IO[Unit] =
+    cm.updateContact(username) { prev =>
+      options.foldLeft(prev) { (acc, flag) =>
+        flag match {
+          case FirstNameFlag(name)     => acc.copy(firstName = name)
+          case LastNameFlag(name)      => acc.copy(lastName = name)
+          case PhoneNumberFlag(number) => acc.copy(phoneNumber = number)
+          case EmailFlag(email)        => acc.copy(email = email)
+          case UnknownFlag(_)          => acc
+        }
+      }
+    } >> IO.println(s"Contact $username updated")
 
-
+  def help: IO[Unit] =
+    IO.println(
+      """
+        |Usage: contacts [command]
+        |
+        |Commands:
+        |  add
+        |  remove <username>
+        |  search id <username>
+        |  search name <name>
+        |  search email <email>
+        |  search number <number>
+        |  view all
+        |  update <username> [flags]
+        |  help
+        |
+        |Flags (for update command):
+        |  --first-name <name>
+        |  --last-name <name>
+        |  --phone-number <number>
+        |  --email <email>
+        |
+        |""".stripMargin
+    )
 }
 
 object Cli {
